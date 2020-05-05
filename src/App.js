9b3e9c5d95004086ec2159321afef254cc3e2039
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-05 12:41:20 +0100

    Replaced TV show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import shawshank from "./Images/shawshank.jpg";
 import "./App.css";
+import peepShow from "./Images/peepShow.jpg";
+import downton from "./Images/downton.jpg";
+import derryGirls from "./Images/derryGirls.jpg";
+import explained from "./Images/explained.jpg";
+import louisT from "./Images/louisT.jpg";
+import skins from "./Images/skins.jpg";
 
 function App() {
   return (
@@ -13,12 +19,12 @@ function App() {
         />
       </header>
       <p className="container">
-        <h1 className="item">Peep Show</h1>
-        <h1 className="item">Explained</h1>
-        <h1 className="item">Downton Abbey</h1>
-        <h1 className="item">Skins</h1>
-        <h1 className="item">Derry Girls</h1>
-        <h1 className="item">Louis Theroux</h1>
+        <img src={peepShow} />
+        <img src={downton} />
+        <img src={derryGirls} />
+        <img src={explained} />
+        <img src={louisT} />
+        <img src={skins} />
       </p>
     </div>
   );
