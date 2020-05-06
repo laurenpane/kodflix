4199580ef11726e65eeca666f63bcfb5a22690e4
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-06 08:36:24 +0100

    Added hover overlay

--- src/App.js
+++ src/App.js
@@ -13,21 +13,27 @@ function App() {
       <div className="container">
         <div className="item">
           <img src={peepShow} />
+          <div className="hoverCover">Peep Show</div>
         </div>
         <div className="item">
           <img src={downton} />
+          <div className="hoverCover">Downton Abbey</div>
         </div>
         <div className="item">
           <img src={derryGirls} />
+          <div className="hoverCover">Derry Girls</div>
         </div>
         <div className="item">
           <img src={explained} />
+          <div className="hoverCover">Explained</div>
         </div>
         <div className="item">
           <img src={louisT} />
+          <div className="hoverCover">Louis Theroux</div>
         </div>
         <div className="item">
           <img src={skins} />
+          <div className="hoverCover">Skins</div>
         </div>
       </div>
     </div>
