3241775741859fd524a8b473cf89afdf9d371d72
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-16 10:23:35 +0100

    Set component state and added link to homepage

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Details.css";
+import { Link } from "react-router-dom";
 
-export default function Details() {
-  return (
-    <div>
-      <h1>Welcome to details page</h1>
-    </div>
-  );
+export default class Details extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message:
+        "Hello, this will be the details page for each Movie & TV show :)",
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ message: "Coming soon! :)" });
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+        <Link className="detailsLink" to="/">
+          Back to homepage
+        </Link>
+      </div>
+    );
+  }
 }
