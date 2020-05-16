ed4dfed7ca9cb2f9b391ff4d0fe273880178cf94
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-16 11:42:44 +0100

    Fixed sizing issue

--- src/Series/Series.js
+++ src/Series/Series.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Series.css";
 
 export default function Series(props) {
   return (
     <Link to={`/${props.id}`} className="item">
-      <img src={props.logo} alt={`${props.name} logo`} />
+      <img id="showLogo" src={props.logo} alt={`${props.name} logo`} />
       <div className="overlay">
         <div className="text">
-          <h2>{props.name}</h2>
+          <h2 id="showName">{props.name}</h2>
         </div>
       </div>
     </Link>
