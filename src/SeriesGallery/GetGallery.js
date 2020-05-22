73857d446a31dccbd923c2e771e32d41866ad2b1
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-22 14:46:04 +0100

    Info changes

--- src/SeriesGallery/GetGallery.js
+++ src/SeriesGallery/GetGallery.js
@@ -12,7 +12,7 @@ export default function getGallery() {
       name: "Peep Show",
       logo: peepShow,
       info:
-        "Set in Croydon. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        'Follow the lives of Mark Corrigan and Jeremy "Jez" Usbourne, two dysfunctional friends who met at the fictional Dartmouth University and now share a flat in Croydon, South London.',
       link: "Back to homepage",
     },
     {
@@ -20,7 +20,7 @@ export default function getGallery() {
       name: "Downton Abbey",
       logo: downton,
       info:
-        "Set in Yorkshire. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Take a journey back in time to post-Edwardian Yorkshire, and follow the lives of the lovable aristocratic Crawley family. Watch as they and their domestic servants endure heartbreak, joy and experience great historical events which in turn affect their lives and the British social hierarchy.",
       link: "Back to homepage",
     },
     {
@@ -28,7 +28,7 @@ export default function getGallery() {
       name: "Derry Girls",
       logo: derryGirls,
       info:
-        "Set in Ireland. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Watch Erin, Orla, Clare and Michelle, and Michelle's English cousin James navigate their teen years during the end of the Troubles in Derry, where they all attend a Catholic girls' secondary school.",
       link: "Back to homepage",
     },
     {
@@ -36,7 +36,7 @@ export default function getGallery() {
       name: "Explained",
       logo: explained,
       info:
-        "American docu-series. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Dig deeper into topical issues from this docuseries, which features short 20-minute episodes that tackle topics, questions and ideas that aren't often part of the daily news cycle, including cryptocurrency, why women are paid less, and astrology.",
       link: "Back to homepage",
     },
     {
@@ -44,7 +44,7 @@ export default function getGallery() {
       name: "Louis Thereoux",
       logo: louisT,
       info:
-        "Englishman sees weird stuff. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Watch award-winning journalist Louis Theroux investigate some of the world's most controversial subcultures, such as survivalists, white supremacists, and porn stars, as he immerses himself in their lifestyle by living amongst them, as he subtly exposes the contradictions and flaws in their beliefs.",
       link: "Back to homepage",
     },
     {
@@ -52,7 +52,7 @@ export default function getGallery() {
       name: "Sex Education",
       logo: sexEd,
       info:
-        "Teenagers learn about love and relationships. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Follow the lives of Otis, Eric, Maeve and their curious classmates as they navigate their way through teenagehood. Otis' mother, Jean Milburn, is a sex therapist, and through a series of events, Otis becomes a paid sex therapist in school for his fellow high students, despite being just as clueless as te rest of them.",
       link: "Back to homepage",
     },
   ];
