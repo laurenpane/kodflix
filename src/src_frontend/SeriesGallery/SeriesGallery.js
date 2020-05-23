4a5adeb21846afc2f43676304ba2b8224ac89216
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-23 17:22:58 +0100

    Appearance changes

--- src/src_frontend/SeriesGallery/SeriesGallery.js
+++ src/src_frontend/SeriesGallery/SeriesGallery.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Series from "../Series/Series.js";
 import getGallery from "./GetGallery.js";
+import "./SeriesGallery.css";
 
 export default function SeriesGallery() {
   return (
     <div>
+      <h1 className="welcome">Hover over an image to discover more...</h1>
       <div className="container">
         {getGallery().map((show) => (
           <Series
