8108e562cae49025942dbb6d671d56ac5d3335e4
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-28 15:05:07 +0100

    Installed npm-run-all so frontend and backend servers work in parallel

--- src/src_backend/App.js
+++ src/src_backend/App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const shows = require("./TVShows");
 
 app.get("/rest/TVShows", (req, res) => res.send(shows.TVShows));
