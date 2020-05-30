d2ade11aea55cc0c43de6d094ae6b168924287c8
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-30 09:31:16 +0100

    Connected the frontend and backend servers

--- src/src_backend/App.js
+++ src/src_backend/App.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const shows = require("./TVShows");
+const fetch = require("node-fetch");
 
-app.get("/rest/TVShows", (req, res) => res.send(shows.TVShows));
+fetch("http://localhost:3001/rest/TVShows")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+app.get("/rest/TVShows", (req, response) => response.send(shows.TVShows));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
