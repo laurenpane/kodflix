eab5b37945bbf88d707b3579307130f0b6f31b0a
Lauren Pane <lauren.pane1@googlemail.com>
2020-05-31 13:08:06 +0100

    Backend change to express.static

--- src/src_backend/App.js
+++ src/src_backend/App.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const shows = require("./TVShows");
 const fetch = require("node-fetch");
 const path = require("path");
 
-app.use(
-  "/static",
-  express.static(path.join(__dirname, "../../build", "index.html"))
-);
+app.use(express.static(path.join(__dirname, "../../build", "index.html")));
 
 fetch("http://localhost:3001/rest/TVShows")
   .then((response) => response.json())
