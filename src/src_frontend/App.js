f149d2e091512ce3ef08069258723849903cb479
Lauren Pane <lauren.pane1@googlemail.com>
2020-06-06 09:25:06 +0100

    Changes to dependencies and fetch

--- src/src_frontend/App.js
+++ src/src_frontend/App.js
@@ -5,6 +5,10 @@ import SeriesGallery from "./SeriesGallery/SeriesGallery.js";
 import Details from "./Details/Details.js";
 import NotFound from "./NotFound.js";
 
+fetch("http://localhost:3001/rest/TVShows")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 export default function App() {
   return (
     <Router>
--- src/src_frontend/SeriesGallery/SeriesGallery.js
+++ src/src_frontend/SeriesGallery/SeriesGallery.js
@@ -1,40 +1,39 @@
 import React from "react";
 import Series from "../Series/Series.js";
-import getGallery from "./GetGallery.js";
 import "./SeriesGallery.css";
 import netflix from "../Images/netflixLogo.png";
-import BackendGallery from "../../src_backend/TVShows.js";
+import TVShows from "../../src_backend/TVShows.js";
 
-/*export default class SeriesGallery extends React.Component {
-  constructor() {
-    super();
-    this.state = { id: {} };
-  }
+// export default class SeriesGallery extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = { show: {} };
+//   }
 
-  render() {
-    return (
-      <div>
-        <img src={netflix} alt="netflix" className="netflixLogo" />
-        <h1 className="welcome">
-          Click or hover over an image to discover more...
-        </h1>
-        <div className="container">
-          {BackendGallery().map((show) => (
-            <Series
-              key={show.id}
-              id={show.id}
-              name={show.name}
-              logo={show.logo}
-              description={show.description}
-              click={show.click}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-*/
+//   render() {
+//     return (
+//       <div>
+//         <img src={netflix} alt="netflix" className="netflixLogo" />
+//         <h1 className="welcome">
+//           Click or hover over an image to discover more...
+//         </h1>
+//         <div className="container">
+//           {this.setState({ show:{ TVShows.map((TVShow) => TVShow.id) } })}
+//           <Series
+//             key={this.state.show.id}
+//             id={this.state.show.id}
+//             name={this.state.show.name}
+//             logo={require(`../Images/${show.id}.jpg`)}
+//             description={this.state.show.description}
+//             click={this.state.show.click}
+//           />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
 export default function SeriesGallery() {
   return (
     <div>
